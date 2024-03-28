8efe67625a578cda593f4c325701fcea735b3b82
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-03-29 01:05:19 +0200

    finished

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-async function getDataFromAPI(search) {
+async function getDataFromAPI(search, page, perPage) {
   const API_KEY = '39362116-97c7d5619fee8bb7db7b12f12';
   const imageType = 'photo';
   const photoOrientation = 'horizontal';
   const safeSearch = true;
+  const per_page = 15;
 
   const params = {
     key: API_KEY,
@@ -12,17 +13,16 @@ async function getDataFromAPI(search) {
     image_type: imageType,
     orientation: photoOrientation,
     safesearch: safeSearch,
+    page,
+    per_page,
   };
 
-try{
-  const response = await axios.get('https://pixabay.com/api/', { params });
-  return response.data
-} 
-catch(error) {
-console.log(error.message)
-
-}
- 
+  try {
+    const response = await axios.get('https://pixabay.com/api/', { params });
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export { getDataFromAPI };
--- src/main.js
+++ src/main.js
@@ -7,25 +7,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getDataFromAPI } from './js/pixabay-api';
 import { createMarkup } from './js/render-functions';
 
-
 // DOM elements
 const formEl = document.querySelector('.search-form');
 const inputEl = formEl.elements.search;
 const gallery = document.querySelector('.gallery');
+let galleryCard;
 const loaderEl = document.querySelector('.loader');
+const loadBtn = document.querySelector('.js-loadbtn');
+
 
 const simplelightbox = new SimpleLightbox('.gallery-item a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    showCounter: false,
-  });
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+});
 
 function loaderToggle() {
   loaderEl.classList.toggle('hidden');
 }
 
+function updateLoadBtnVisibility() {
+  if (renderedHits < totalHits) {
+    loadBtn.classList.remove('hidden');
+  } else {
+    loadBtn.classList.add('hidden');
+    showError("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+function resetForPagination() {
+  currentPage = 1;
+  renderedHits = 15;
+}
+
 function cleanGallery() {
   if (gallery.innerHTML) gallery.innerHTML = '';
+}
+
+function smoothScroll(){
+  const cardHeight = galleryCard.getBoundingClientRect().height;
+  window.scrollBy({
+    top: 3 * cardHeight, 
+    behavior: 'smooth' 
+  });
 }
 
 function showError(message) {
@@ -35,24 +59,40 @@ function showError(message) {
   });
 }
 
-async function getPhotoFromAPI(query) {
-    const data = await getDataFromAPI(query);
+let currentPage = 1;
+let renderedHits = 15;
+let currentQuery;
+let totalHits;
+
+async function getPhotoFromAPI(query, page) {
+  try {
+    const data = await getDataFromAPI(query, page);
+
+    totalHits = data.totalHits;
+
     if (!data.hits.length) {
       showError(
         'Sorry, there are no images matching your search query. Please try again!'
       );
       return;
     }
-    gallery.innerHTML = createMarkup(data.hits);
+    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    simplelightbox.refresh();
+    updateLoadBtnVisibility();
+    
+  } catch (error) {
+    showError('Sorry, something wrong happened while fetching.');
+    console.error(error);
+  } finally {
+    loaderToggle();
   }
-  
+}
 
 formEl.addEventListener('submit', searchHandler);
 function searchHandler(evt) {
   evt.preventDefault();
 
   cleanGallery();
-
   loaderToggle();
 
   if (!inputEl.value.trim()) {
@@ -60,18 +100,29 @@ function searchHandler(evt) {
     showError('Please enter something!');
     return;
   }
-  try {
-    getPhotoFromAPI(inputEl.value);
-    if (simplelightbox) simplelightbox.refresh();
-   
-  } 
-  catch (error) {
-    showError('Sorry, something wrong happened while fetching.');
 
-    console.log(error);
-  }
-   
-loaderToggle()
+  currentQuery = inputEl.value;
+
+  getPhotoFromAPI(currentQuery, currentPage);
   
 }
 
+loadBtn.addEventListener('click', loadMoreHandler);
+
+function loadMoreHandler() {
+  if (renderedHits >= totalHits) {
+    resetForPagination();
+    return;
+  }
+  currentPage += 1;
+  renderedHits += 15;
+
+  loaderToggle();
+  
+  galleryCard = document.querySelector('.gallery-item');
+  getPhotoFromAPI(currentQuery, currentPage).then(()=>{
+   
+    smoothScroll()
+  })
+
+}
